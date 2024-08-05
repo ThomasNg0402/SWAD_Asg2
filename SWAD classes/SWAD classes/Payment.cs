2b39bc4725333ea9d943f1cf033ed9d8a7433576
DefinitelyNotAnt <150310592+DefinitelyNotAnt@users.noreply.github.com>
2024-08-06 02:43:54 +0800

    reservecar

--- SWAD classes/SWAD classes/Payment.cs
+++ SWAD classes/SWAD classes/Payment.cs
@@ -9,9 +9,18 @@ namespace SWAD_classes
     class Payment
     {
         public int PaymentId { get; set; }
-        public int Amount { get; set; }
+        public decimal Amount { get; set; }
         public DateTime TimePaid { get; set; }
         public DateTime DatePaid { get; set; }
         public string Status { get; set; }
+        public bool AttemptPayment(decimal amt)
+        {
+            // Fake contact system here, then return true on success
+            this.Amount = amt;
+            this.TimePaid = DateTime.Now;
+            this.DatePaid = DateTime.Now;
+            this.Status = "Paid";
+            return true;
+        }
     }
 }
