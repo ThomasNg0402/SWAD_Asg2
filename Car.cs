7999e6117132f7736215238bf33fc3dced5eb208
supnie <150310259+SupNie@users.noreply.github.com>
2024-08-05 21:47:39 +0800

    Update again

--- Car.cs
+++ Car.cs
@@ -1,22 +1,27 @@
-class Car
+using System;
+
+namespace SWAD_Asg2
 {
-    public int CarId { get; set; }
-    public string Make { get; set; }
-    public string Model { get; set; }
-    public int Mileage { get; set; }
-    public int Year { get; set; }
-    public string Photo { get; set; }
-    public string CarType { get; set; }
-    public int RentalRate { get; set; }
-    private List<Registration> regList;
-
-    public void addRegistration(Registration reg)
+    class Car
     {
-        this.reg = reg;
-    }
-    public void updateCarRegistered(int aCar)
-    {
-        this.aCar += aCar;
-    }
+        public int CarId { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Mileage { get; set; }
+        public int Year { get; set; }
+        public string Photo { get; set; }
+        public string CarType { get; set; }
+        public int RentalRate { get; set; }
+        private List<Registration> regList;
 
+        public void addRegistration(Registration reg)
+        {
+            this.reg = reg;
+        }
+        public void updateCarRegistered(int aCar)
+        {
+            this.aCar += aCar;
+        }
+
+    }
 }
