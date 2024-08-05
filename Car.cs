c9330177de3c9268aacc36e081663cd0e67b0eab
supnie <150310259+SupNie@users.noreply.github.com>
2024-08-05 20:51:12 +0800

    Update my code

--- Car.cs
+++ Car.cs
@@ -1,4 +1,22 @@
 class Car
 {
+    public int CarId { get; set; }
+    public string Make { get; set; }
+    public string Model { get; set; }
+    public int Mileage { get; set; }
+    public int Year { get; set; }
+    public string Photo { get; set; }
+    public string CarType { get; set; }
+    public int RentalRate { get; set; }
+    private List<Registration> regList;
+
+    public void addRegistration(Registration reg)
+    {
+        this.reg = reg;
+    }
+    public void updateCarRegistered(int aCar)
+    {
+        this.aCar += aCar;
+    }
 
 }
