480e3eda72395766c1767b0b733055179e38b7e1
supnie <150310259+SupNie@users.noreply.github.com>
2024-08-05 23:14:11 +0800

    Add files via upload

---
+++ SWAD classes/SWAD classes/Insurance.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SWAD_classes
+{
+    class Insurance
+    {
+        private int insuranceId;
+        private string provider;
+        private string coverageDetails;
+        public int InsuranceId { get; set; }
+        public string Provider { get; set; }
+        public string CoverageDetails { get; set; }
+        private CarOwner myCarOwner;
+        public CarOwner MyCarOwner
+        {
+            set
+            {
+                if (myCarOwner != value)
+                  {
+                    myCarOwner = value;
+                    value.MyCarInsurance = this;
+                }
+            }
+        }
+        public void add(string aCarOwner, int aCar)
+        {
+            this.aCarOwner = aCarOwner;
+            this.aCar += aCar;
+        }
+
+    }
+}
